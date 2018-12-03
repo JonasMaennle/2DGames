6964d9586bf33523389d7eece80c2fd148658bb8
JonasMaennle <jonas.maennle@gmx.de>
2018-12-03 18:50:23 +0100

    comm

--- Performance_Shooter/src/object/Goal.java
+++ Performance_Shooter/src/object/Goal.java
@@ -8,13 +8,12 @@ import static helpers.Setup.*;
 import java.awt.Rectangle;
 
 import Enity.Entity;
-import shader.Light;
 
 public class Goal implements Entity{
 
 	private Image image;
 	private float x, y, width, height;
-	private Light light;
+	//private Light light;
 	
 	public Goal(float x, float y)
 	{
@@ -23,14 +22,14 @@ public class Goal implements Entity{
 		this.width = TILE_SIZE;
 		this.height = TILE_SIZE;
 		this.image = quickLoaderImage("objects/goal");
-		this.light = new Light(new Vector2f(x + MOVEMENT_X, y + MOVEMENT_Y), 255, 51, 255, 14f);
-		lights.add(light);
+		//this.light = new Light(new Vector2f(x + MOVEMENT_X, y + MOVEMENT_Y), 255, 51, 255, 14f);
+		//lights.add(light);
 	}
 
 	@Override
 	public void update() 
 	{
-		light.setLocation(new Vector2f(x + MOVEMENT_X + 32, y + MOVEMENT_Y + 32));
+		//light.setLocation(new Vector2f(x + MOVEMENT_X + 32, y + MOVEMENT_Y + 32));
 	}
 
 	@Override
